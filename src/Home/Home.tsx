d028cce3398d3ebd8ccca0c1b9fda7498e9fe6f6
Rakeen Huq <r2huq@uwaterloo.ca>
2025-05-26 15:58:06 -0400

    added dedicated hours and location components and used in About and Home and Contact

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Hours from '../Hours/Hours'
 import StoreFront from '/front.jpg'
 import Wrap1 from '/wrap1.jpg'
 
@@ -103,48 +104,7 @@ function Home() {
         </div>
 
         {/* Hours Section */}
-        <div className='
-          flex
-          flex-col
-          items-center
-          mb-8
-        '>
-          <h2 className='
-            text-2xl
-            font-semibold
-            text-gray-800
-            mb-4
-          '>
-            Hours
-          </h2>
-          <div className='
-            grid
-            grid-cols-2
-            gap-x-4
-            gap-y-4
-            text-gray-600
-            text-md
-            lg:text-lg
-            leading-relaxed
-            w-full
-            max-w-md
-          '>
-            <div className='font-medium text-gray-800 text-right'>Monday:</div>
-            <div>11:00 AM - 10:00 PM</div>
-            <div className='font-medium text-gray-800 text-right'>Tuesday:</div>
-            <div>11:00 AM - 10:00 PM</div>
-            <div className='font-medium text-gray-800 text-right'>Wednesday:</div>
-            <div>11:00 AM - 10:00 PM</div>
-            <div className='font-medium text-gray-800 text-right'>Thursday:</div>
-            <div>11:00 AM - 10:00 PM</div>
-            <div className='font-medium text-gray-800 text-right'>Friday:</div>
-            <div>11:00 AM - 12:00 AM</div>
-            <div className='font-medium text-gray-800 text-right'>Saturday:</div>
-            <div>11:00 AM - 12:00 AM</div>
-            <div className='font-medium text-gray-800 text-right'>Sunday:</div>
-            <div>11:00 AM - 10:00 PM</div>
-          </div>
-        </div>
+        <Hours />
 
         {/* Reviews Section */}
         <div className='
